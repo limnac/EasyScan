49e00bd6cd30d484229bb7b713e317012847d6f3
limnac <huchuang@hotmail.com>
2023-05-18 20:57:10 +0800

    feat:加入扫描组件

---
+++ app/src/main/java/com/limnac/easyscan/MainActivity.java
@@ -0,0 +1,32 @@
+package com.limnac.easyscan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.QuickContactBadge;
+import android.widget.Toast;
+
+import com.limnac.easyscan.Activity.ScanActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btnScan = findViewById(R.id.btn_scan);
+        btnScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ScanActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
